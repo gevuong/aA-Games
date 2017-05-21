40b94c582bbaa814b5b02592168d14bf0a079162
George V <georgevuong18@gmail.com>
2017-05-21 16:13:20 -0700

    trying to figure out how to properly implement class methods to pass first 5 specs in mastermind

--- 341a4778-georgevuong18-mastermind/lib/mastermind.rb
+++ 341a4778-georgevuong18-mastermind/lib/mastermind.rb
@@ -1,5 +1,23 @@
 class Code
   attr_reader :pegs
+  attr_accessor :PEGS, :parse, :random
+
+  def initialize(pegs = [])
+    #raise "Error" if pegs.class != Array
+    @pegs = pegs
+  end
+
+  def self.PEGS
+    "string"
+  end
+
+  def self.parse(str)
+    #puts str
+  end
+
+  def self.random
+  end
+
 end
 
 class Game
