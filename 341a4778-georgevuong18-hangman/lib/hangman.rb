4200e1e5eb072791b74363ec9481fc25bafad449
George V <georgevuong18@gmail.com>
2017-05-24 11:30:40 -0700

    completed Phase 1 of 3 in hangman

--- 341a4778-georgevuong18-hangman/lib/hangman.rb
+++ 341a4778-georgevuong18-hangman/lib/hangman.rb
@@ -1,32 +1,29 @@
 class Hangman
-  attr_reader :guesser, :referee#, :board
+  attr_reader :guesser, :referee, :board
 
-  def initialize(players)
-    # players is a hash of symbols/players, which contains data for each player (:referee and :guesser)
-    @players = players
-  end
-
-  def guesser
-    @players[:guesser]
-    #p @length
-  end
-
-  def referee
-    @players[:referee]
-  end
-
-  def board
-    @board # returns nil, which is the state of the "guessing word"?
+  def initialize(players = {})
+    # players is an options hash of symbols/players, which contains data for each player (:referee and :guesser)
+    @guesser = players[:guesser]
+    @referee = players[:referee]
   end
 
   def setup
-      length = referee.pick_secret_word
-      #p @length
-     #p guesser.register_secret_length = referee.pick_secret_word
+    secret_word_length = @referee.pick_secret_word
+    @guesser.register_secret_length(secret_word_length)
+    @board = Array.new (secret_word_length)
+
   end
 
   def take_turn
+    letter = @guesser.guess # asks guesser to guess
+    @referee.check_guess(letter) # has referee check guesser's guess
+    self.update_board
+    @guesser.handle_response
   end
+
+  def update_board(*args)
+  end
+  
 end
 
 
@@ -45,18 +42,19 @@ class ComputerPlayer
   def initialize(dictionary)
     # @dictionary returns an array of a string
     @dictionary = dictionary
+
   end
 
   def pick_secret_word
     @dictionary.join.length
   end
 
-  def register_secret_length(length)
-    p length
+  def register_secret_length(secret_word_length)
+    #secret_word.length
   end
 
   def guess(board)
-    p board.to_s[0]  # board is an array of nil values, length 6
+    #board.to_s[0] # board is an array of nil values, length 6
   end
 
   def check_guess(letter)
