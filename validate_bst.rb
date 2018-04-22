f82c5978d69004634783c4057e518b2206ea38e9
George V <georgevuong18@gmail.com>
2018-04-22 13:43:11 -0700

    add validate_bst iterataive

--- validate_bst.rb
+++ validate_bst.rb
@@ -25,18 +25,83 @@
 
 
 class BTNode
-    attr_accessor :val, :left, :right
+    attr_reader :val
+    attr_accessor :left, :right
     def initialize(val)
         @val = val
-        @left = nil
-        @right = nil
+        @left, @right = nil, nil
     end
 end
 
-def is_valid_bst(root)
-    # return true if root.nil? because bst only contains a root
+# iterative solution
+# assume all nodes are not unique. Plan of Action: create in order traversal. Prior to pushing value into in_order array, compare current_nodes value with min value in in_order array
+def is_valid_bst?(root)
+    return true if root.nil? # based on leetCode test case, return true even if root is nil
+    
+    # initialize an empty stack and current_node 
+    stack = []
+    in_order_arr = []
+    node_count = 0
+    current_node = root
 
-    # for each root, I need to find min and max child node, assuming root has children
+    while true
+        # push left_child into stack until left_child is nil. This means you have 
+        if current_node
+            stack.push(current_node) # possible optimization: if current_node = prev_node
+            
+            prev_node = current_node 
+            return false if current_node.val <= prev_node.val
 
-    # then compare root with min_value, and root with max_value
+            left_child = current_node.left 
+            current_node = left_child
+
+        else # de-stack and update current_node as right_child
+            return true if stack.empty?
+            root = stack.pop
+
+            # initialize in_order_arr and min_val because we have reached leftmost node
+            if node_count == 0 
+                in_order_arr.push(root.val)
+
+                right_child = root.right
+                current_node = right_child
+                prev_node = root # set prev_node as current_node
+                node_count += 1
+                next 
+            end 
+
+            return false if root.val <= prev_node.val
+            in_order_arr.push(root.val)
+
+            prev_node = root
+            right_child = root.right
+            current_node = right_child
+        end 
+    end 
 end
+
+
+# recursive solution
+def is_valid_bst_rec?(root)
+
+end 
+
+
+node_1 = BTNode.new(1)
+node_2 = BTNode.new(2)
+node_3 = BTNode.new(3)
+node_4 = BTNode.new(4)
+node_5 = BTNode.new(5)
+node_6 = BTNode.new(5)
+
+node_4.left = node_2
+node_2.left = node_1
+node_2.right = node_3
+node_4.right = node_5
+node_5.left = node_6
+
+p is_valid_bst?(node_5) # == false
+# p is_valid_bst?(node_4) # == true
+# p is_valid_bst?(node_2) # == true
+# p is_valid_bst?(node_3) # == true
+
