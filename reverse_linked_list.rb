d683e3c901a3a930a7f091c48af1193c30113fec
George V <georgevuong18@gmail.com>
2018-04-26 17:52:11 -0700

    update reverse_linked_list

--- reverse_linked_list.rb
+++ reverse_linked_list.rb
@@ -1,3 +1,5 @@
+# Reverse a singly linked list
+
 class Node  
     attr_accessor :next  
     attr_reader :val 
@@ -16,17 +18,26 @@ class LinkedList
 end 
 
 
+# Time: O(n), where n is number of Nodes in LL
+# Space: O(1), because we are only storing one node in a variable
 def reverse_LL(head)
     current = head
     prev = nil
 
     while current 
+        # store next_node before changing its reference to prev
         next_node = current.next
+        
+        # change current's NEXT POINTER to point to its previous element. To be clear, you're not modifying the next Node in any way to equal prev, only current's NEXT POINTER. 
+        # In other words, assuming all nodes point to the right beginning at the head, you're setting current.next to point to the left of head, which is nil, or prev.
         current.next = prev 
+
+        # before re-entering while loop, set prev to current, and current to next_node to traverse LL
         prev = current 
-        current - next_node
+        current = next_node
     end
     
+    # return new head
     prev
 end 
 
